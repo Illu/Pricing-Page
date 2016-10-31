544e6319da37b0dedaa9f5ed0da0b663d5e5e011
Maxime Nory <maximenory@yahoo.fr>
2016-10-31 12:10:09 +0100

    Selection
    the total price and discount is adjusted when the user uses the slider.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
-// function outputUpdate(vol) {
-//   var output = document.querySelector("#quantity");
-// 	output.value = vol;
-//   output.style.left = vol + 'px';
-// }
+function discount(total, discount){
+  return total - (total * (discount / 100));
+}
+
+function outputUpdate(quantity){
+  var units = ['A', 'B', 'C'];
+  var total = 0;
+  $('#quantity').html(quantity);
+  for (var i = 0; i < units.length; i++){
+    if (quantity >= 50)
+      $('#discount' + units[i]).html('15%');
+    else
+      $('#discount' + units[i]).html('0%');
+    total = $('#unit' + units[i]).text().slice(1);
+    total *= quantity;
+    var dis = $('#discount' + units[i]).text().slice(0, -1);
+    total = discount(total, dis).toFixed(2);
+    $('#total' + units[i]).html('$' + total);
+  }
+}
